d81190736913446678832a7555d66b6facd860a2
James <10740291+Electrocyte@users.noreply.github.com>
2022-06-12 16:10:28 +0800

    Create install-notes.groovy

---
+++ install-notes.groovy
@@ -0,0 +1,23 @@
+# first time setup in repo
+gh repo clone Electrocyte/weather
+cd weather
+pipenv install pandas jupyter seaborn matplotlib
+pipenv shell # activate [VE] virtual environment for jupyter notebook
+git add . 
+git commit
+git push
+
+pipenv install statsmodels
+git add . 
+git commit
+git push
+
+
+# new computer
+gh repo clone Electrocyte/weather
+pipenv install
+pipenv shell
+
+# to run
+pipenv shell
+jupyter-notebook
